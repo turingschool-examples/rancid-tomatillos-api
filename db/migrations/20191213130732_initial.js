f2b3e8a692792646776bf02b29f7f2a9f454dbb3
Robbie Jaeger <jaeger.rob@gmail.com>
2019-12-13 16:10:04 -0700

    Create inital migration

---
+++ db/migrations/20191213130732_initial.js
@@ -0,0 +1,36 @@
+
+exports.up = function(knex) {
+  return Promise.all([
+    knex.schema.createTable('users', function(table) {
+      table.increments('id').primary();
+      table.string('name');
+      table.string('email');
+      table.string('password');
+    }),
+
+    knex.schema.createTable('movies', function(table) {
+      table.increments('id').primary();
+      table.string('title');
+      table.string('poster_path');
+      table.string('release_date');
+      table.string('overview');
+      table.timestamps(true, true);
+    }),    
+
+    knex.schema.createTable('moviesReviews', function(table) {
+      table.increments('id').primary();
+      table.integer('user_id').unsigned().references('users.id');
+      table.integer('movie_id').unsigned().references('movies.id');
+      table.integer('rating');
+      table.timestamps(true, true);
+    })
+  ])
+};
+
+exports.down = function(knex) {
+  return Promise.all([
+    knex.schema.dropTable('moviesReviews'),
+    knex.schema.dropTable('movies'),
+    knex.schema.dropTable('users')
+  ]);
+};
--- server.js
+++ server.js
@@ -4,7 +4,11 @@ const app = express();
 app.set('port', process.env.PORT || 3000);
 
 app.get('/', (requst, response) => {
-  response.status(200).json({hello: 'Head on over to /api/v1/movies to start getting movies and go from there'});
+  response.status(200).json({hello: 'Head on over to /api/v1/movies to start getting movies'});
+});
+
+app.get('api/v1/movies', (request, resoonse) => {
+  
 });
 
 app.listen(app.get('port'), () => {
