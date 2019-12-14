dc567c4bf1fed73ba581dcb424024ad05283c8ad
Robbie Jaeger <jaeger.rob@gmail.com>
2019-12-14 10:48:57 -0700

    Add POST for user login without error check

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 
 app.set('port', process.env.PORT || 3000);
+app.use(express.json());
 
 const environment = process.env.NODE_ENV || 'development';
 const configuration = require('./knexfile')[environment];
@@ -9,6 +10,18 @@ const database = require('knex')(configuration);
 
 app.get('/', (request, response) => {
   response.status(200).json({hello: 'Head on over to /api/v1/movies to start getting movies'});
+});
+
+app.post('/api/v1/users', (request, response) => {
+  // Check body contents
+  const { email, password } = request.body;
+
+  database('users').where({ email, password }).first()
+    .then(user => {
+      const { id, name, email } = user;
+      return response.status(201).json({ user: { id, name, email } });
+    })
+    .catch(err => response.status(500).json({ err }));
 });
 
 app.get('/api/v1/movies', (request, response) => {
