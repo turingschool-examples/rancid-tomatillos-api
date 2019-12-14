6fb557146d5747e27019a455ab12607389eae762
Robbie Jaeger <jaeger.rob@gmail.com>
2019-12-13 21:44:01 -0700

    Add knex environment config and all movies endpoint

--- server.js
+++ server.js
@@ -3,12 +3,18 @@ const app = express();
 
 app.set('port', process.env.PORT || 3000);
 
-app.get('/', (requst, response) => {
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('./knexfile')[environment];
+const database = require('knex')(configuration);
+
+app.get('/', (request, response) => {
   response.status(200).json({hello: 'Head on over to /api/v1/movies to start getting movies'});
 });
 
-app.get('api/v1/movies', (request, resoonse) => {
-  
+app.get('/api/v1/movies', (request, response) => {
+  database('movies').select()
+    .then(movies => response.status(200).json({ movies }))
+    .catch(err => response.status(500).json({ error: err }));
 });
 
 app.listen(app.get('port'), () => {
