aa5a537594b8e5e2c638fb6d43254fa3783bc47e
Robbie Jaeger <jaeger.rob@gmail.com>
2020-04-14 15:44:24 -0600

    Refactor movie seed file to not be dependent on order of fetch calls as information is added

--- db/seeds/dev/2-movies.js
+++ db/seeds/dev/2-movies.js
@@ -3,41 +3,51 @@
 const fetch = require('node-fetch');
 require('dotenv').config();
 
-const fetchMovies = () => {
+const fetchMoviesAndSupportingData = () => {
   const pages = [1, 2];
-
-  const moviePagePromises = pages.map(pageNum => {
+  // flattened array of movies
+  const moviePagesPromise = Promise.all(pages.map(pageNum => {
     return fetch(`https://api.themoviedb.org/3/movie/now_playing?api_key=${process.env.MOVIE_DB_APIKEY}&language=en-US&page=${pageNum}`)
       .then(response => response.json())
       .then(movieData => movieData.results)
+  }))
+  .then(moviePages => [].concat.apply([], moviePages));
+
+  // array of genres and associated ids
+  const genresPromise = fetch(`https://api.themoviedb.org/3/genre/movie/list?api_key=${process.env.MOVIE_DB_APIKEY}&language=en-US`)
+    .then(response => response.json())
+    .then(genreData => genreData.genres);
+
+  return Promise.all([moviePagesPromise, genresPromise]);
+};
+
+const buildMovieData = moviesAndSupportingData => {
+  const [movies, genres] = moviesAndSupportingData;
+
+  return movies.map(movie => {
+    const { id, title, overview, release_date } = movie;
+
+    const full_poster_path = 'https://image.tmdb.org/t/p/original/' + movie.poster_path;
+    let full_backdrop_path = 'https://image.tmdb.org/t/p/original/' + movie.backdrop_path;
+
+    if (!movie.backdrop_path) {
+      full_backdrop_path = 'https://www.esm.rochester.edu/uploads/NoPhotoAvailable.jpg';
+    }
+
+    return {
+      id,
+      poster_path: full_poster_path,
+      backdrop_path: full_backdrop_path,
+      title,
+      overview,
+      release_date
+    };
   });
-
-  return Promise.all(moviePagePromises)
-    .then(moviePages => {
-      return [].concat.apply([], moviePages).map(movie => {
-        const { id, title, overview, release_date } = movie;
-
-        const full_poster_path = 'https://image.tmdb.org/t/p/original/' + movie.poster_path;
-        let full_backdrop_path = 'https://image.tmdb.org/t/p/original/' + movie.backdrop_path
-
-        if (!movie.backdrop_path) {
-          full_backdrop_path = 'https://www.esm.rochester.edu/uploads/NoPhotoAvailable.jpg';
-        }
-
-        return {
-          id,
-          poster_path: full_poster_path,
-          backdrop_path: full_backdrop_path,
-          title,
-          overview,
-          release_date
-        };
-      });
-    });
 };
 
 exports.seed = (knex) => {
-  return fetchMovies()
+  return fetchMoviesAndSupportingData()
+    .then(moviesAndSupportingData => buildMovieData(moviesAndSupportingData))
     .then(cleanedMovies => {
       return knex('movies').insert(cleanedMovies);
     })
