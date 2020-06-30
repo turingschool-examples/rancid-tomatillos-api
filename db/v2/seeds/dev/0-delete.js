ebafdf76b22381a4c7ef98b4f4df1e04dd4522c5
Khalid Williams <khalidwill27@gmail.com>
2020-06-30 09:51:31 -0600

    Delete videos on seed

--- db/v2/seeds/dev/0-delete.js
+++ db/v2/seeds/dev/0-delete.js
@@ -1,6 +1,7 @@
 
 exports.seed = function(knex) {
   return knex('usersReviews').del()
+    .then(() => knex('movies_videos').del())
     .then(() => knex('movies').del())
     .then(() => knex('users').del())
 };
